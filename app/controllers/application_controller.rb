967feaac3ab7941bd948a0173a23eb03164b5a14
Christerle Nounoute <christerlesn@gmail.com>
2021-02-17 05:53:46 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,17 +23,17 @@ post '/recipes' do
 end
 
 get '/recipes/:id' do
-  @recipe = Recipe.find_by(params[:id])
+  @recipe = Recipe.find(params[:id])
   erb :show
 end
 
 get '/recipes/:id/edit' do
-  @recipe = Recipe.find_by(params[:id])
+  @recipe = Recipe.find(params[:id])
   erb :edit
 end
 
 patch '/recipes/:id' do
-  @recipe = Recipe.find_by(params[:id])
+  @recipe = Recipe.find(params[:id])
   @recipe.name = params[:name]
   @recipe.ingredients = params[:ingredients]
   @recipe.cook_time = params[:cook_time]
